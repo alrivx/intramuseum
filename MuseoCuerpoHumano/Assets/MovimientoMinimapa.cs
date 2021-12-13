d795f4cd3656f04f944bf6db1fa0c3d070bffdec
Saul Axel <saulaxel.50@gmail.com>
2021-12-12 21:13:53 -0600

    minimapa

---
+++ MuseoCuerpoHumano/Assets/MovimientoMinimapa.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoMinimapa : MonoBehaviour
+{
+    public Transform playerTransform;
+    public Transform mainCamera;
+    public float altura = 20.0f;
+
+    private Vector3 normalXZ = new Vector3(0, 1, 0);
+    private float posicionYCamara;
+  
+
+    private void Start()
+    {
+        // La cámara del minimapa estará encima del usuario
+        // Su posición "Y" solo se inicializa una vez para que
+        // no le afecten los saltos del usuario
+        posicionYCamara = playerTransform.position.y + altura;
+    }
+
+    void LateUpdate()
+    {
+        // Esto se ejecuta en la cámara del minimapa.
+        // Queremos que el minimapa tenga la posición del jugador
+        // pero más alto
+        Vector3 newPosition = playerTransform.position;
+        newPosition.y = posicionYCamara;
+
+        // Por su parte, dicha cámara debe apuntar en la misma
+        // dirección que la cámara principal pero siempre hacia abajo
+        float newYRotation = mainCamera.transform.rotation.eulerAngles.y;
+        Vector3 newRotationEuler = new Vector3(90.0f, newYRotation, 0);
+        Quaternion newRotation = new Quaternion();
+        newRotation.eulerAngles = newRotationEuler;
+
+        transform.rotation = newRotation;
+        transform.position = newPosition;
+    }
+}
